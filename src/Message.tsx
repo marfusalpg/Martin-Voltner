faee6e62bd4227a9ec1a8e8198f8f888f8c29025
marfusalpg <voltner.martin@gmail.com>
2026-08-25 16:27:10 +0200

    HUGE WEBSITE UPDATE
    HTML - > REACT
    New website

---
+++ src/Message.tsx
@@ -0,0 +1,15 @@
+
+// PascalCasing
+function Message(){
+
+    const name = 'Fusha'
+    if (name) {
+        return <h1>Hello {name}!</h1>
+    }
+    else {
+        return <h1>Hello World!</h1>
+    }
+}
+
+
+export default Message;
