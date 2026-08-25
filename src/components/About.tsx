6f76ea28f308d590ca8b17a12d745abb3dde25e9
marfusalpg <voltner.martin@gmail.com>
2026-08-25 18:29:19 +0200

    s

---
+++ src/components/About.tsx
@@ -0,0 +1,30 @@
+const About = () => (
+  <section id="about" className="about">
+    <p className="section-kicker">/ About me</p>
+
+    <div className="about-copy">
+      <p className="headline">
+        I build thoughtful software, playful visuals and clean digital
+        experiences.
+      </p>
+
+      <div className="about-grid">
+        <p>
+          Hi, I&apos;m <span className="highlight">Martin</span>. I am a
+          BSc student in Computer Science at Palacký University Olomouc and a
+          passionate software developer. My areas of interest include Web
+          Development, creating Windows applications, computer games and 3D
+          modeling.
+        </p>
+
+        <p>
+          I enjoy crafting practical solutions that feel polished and useful,
+          whether I&apos;m working on an interface, a visual asset or a
+          creative prototype.
+        </p>
+      </div>
+    </div>
+  </section>
+);
+
+export default About;
---
+++ src/data/content.ts
@@ -0,0 +1,47 @@
+export const education = [
+  {
+    title: "BSc., Computer Science",
+    location: "Palacký University Olomouc",
+    detail: "Programming & Software Development",
+    period: "2025-Now",
+  },
+  {
+    title: "High School",
+    location: "Gymnázium Lesní čtvrť",
+    detail: "",
+    period: "2021-2025",
+  },
+];
+
+export const employment = [
+  {
+    title: "Freelance",
+    location: "3D Modeling",
+    detail: "",
+    period: "2024 - Now",
+  },
+  {
+    title: "Fushix_",
+    location: "3D Designer, Graphical Designer",
+    detail: "Sidehustle",
+    period: "2020 - Now",
+  },
+];
+
+export const contactItems = [
+  {
+    label: "E-mail",
+    value: "voltner.martin@gmail.com",
+    href: "mailto:voltner.martin@gmail.com",
+  },
+  {
+    label: "Instagram",
+    value: "fushin_bum",
+    href: "https://www.instagram.com/fushin_bum/",
+  },
+  {
+    label: "Discord",
+    value: "fushin_bum",
+    href: undefined,
+  },
+];
